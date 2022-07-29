7e73794df07f96c97f3d12fb991b2f2b61a4bdeb
Phil Daum <philgdaum@gmail.com>
2022-07-28 23:52:55 -0700

    start page making

---
+++ web-app/js/seeds.js
@@ -0,0 +1,23 @@
+//This will be the array of all of the stuff from the seed spreadsheet!
+const userTexts = [
+    //0 
+    ["hi", "hey", "hello", "good morning", "good afternoon", "good day"],
+    //1
+    ["how are you", "how is life", "how are things", "how are you doing", 
+    "are you doing good", "are you fine", "how is your day going", "how is your day", 
+    "what's up", "whats up", "you good"],
+    //2
+    ["what are you doing", "what is going on", "what is up", "how is your day", 
+    "what's up", "whats up", "you good"],
+    //3
+    ["how old are you", "are you old"],
+    //4
+    ["who are you", "are you human", "are you bot", "are you human or bot"],
+    //5
+    ["who created you", "who made you", "were you created"],
+    //goes to my LinkedIn
+    ["linkedin", "resume"],
+    //goes to my Github
+    ["github", "programming", "developer", "dev", "coding", "git"]
+]
+
